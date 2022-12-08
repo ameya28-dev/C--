74fd9abe03d0d2f85302be3e3a5d9e4f41753f41
Ameya G Mahale <ameya28.mahale@gmail.com>
2022-12-09 02:05:50 +0530

    Added setBit Method

--- BitManipulation.cpp
+++ BitManipulation.cpp
@@ -7,10 +7,14 @@ int getBit(int n, int pos){
     return (n & (1<<pos)) != 0;
 } 
 
+int setBit(int n, int pos){
+    return (n | (1 << pos));
+}
+
 int main(){
 
-    cout << getBit(5,1); //0101 1010 0000
-
+    cout << "Get Bit: " << getBit(5,1) << endl; //0101 1010 0000
+    cout << "Set Bit: " << setBit(5,1) << endl;
 
     return 0;
 }
