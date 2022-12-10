111f1bc1e8db656949afede9a371ed1654f56bca
Ameya G Mahale <ameya28.mahale@gmail.com>
2022-12-10 17:49:30 +0530

    2 unique numebrs function

--- BitManipulation.cpp
+++ BitManipulation.cpp
@@ -46,6 +46,37 @@ void generateSubsets(int arr[], int n){
     }
 }
 
+int findUniqueNumber(int arr[], int n){
+    int xorsum = 0;
+    for(int i = 0; i<n; i++){
+        xorsum ^= arr[i];
+    }
+    return xorsum;
+}
+
+void find2UniqueNumbers(int arr[], int n){
+    // Get XOR of all numbers
+    int xorsum = 0;
+    for(int i = 0; i<n; i++){
+        xorsum ^= arr[i];
+    }
+
+    // find postion of rightmost set bit in xorsum
+    int setbit = 1, pos = 0;
+    while(!(xorsum & setbit)){
+        pos++;
+        setbit <<= 1;
+    }
+    
+    // find 2 unique numbers
+    int xorsum2 = 0;
+    for(int i = 0; i<n; i++){
+        if (getBit(arr[i],pos)) xorsum2 ^= arr[i];
+    }
+
+    cout << xorsum2 << "\t" << (xorsum2 ^ xorsum);    
+}
+
 int main(){
 
     cout << "Get Bit: " << getBit(5,1) << endl; //0101 1010 0000
@@ -62,9 +93,18 @@ int main(){
     cout << "Number of ones in 0: " << numberOfOnes(0) << endl << endl;
 
     // Generate all possible subsets of a set
-    int arr[3] = {1,2,3};
+    int setarray[3] = {1,2,3};
     cout << "All subsets: " << endl;
-    generateSubsets(arr, 3);
+    generateSubsets(setarray, 3);
+
+    // Find number which appears once in an array while the rest appear twice
+    int unique1array[] = {1,7,2,2,1};
+    cout << "Find unique one: " << findUniqueNumber(unique1array, 5) << endl;
+
+    // Find 2 numbers which appears once in an array while the rest appear twice
+    int unique2array[] = {2,99,8,7,8,2};
+    cout << "Find 2 unique numbers: ";
+    find2UniqueNumbers(unique2array, 6);
 
     return 0;
 }
