5e0211871596170a537773f401c2e4b252cd3d99
Ameya G Mahale <ameya28.mahale@gmail.com>
2022-12-11 12:12:58 +0530

    gcd method

--- InclusionExclusion.cpp
+++ InclusionExclusion.cpp
@@ -11,11 +11,21 @@ int divisible(int n, int a, int b){
     return c1+c2-c3;
 }
 
+int gcd(int m, int n){
+    if(n == 0) return m;
+    else return(gcd(n,m%n));
+}
+
 int main(){
-    int n,a,b;
-    cout << "Enter Input:" << endl;
-    cin >> n >> a >> b; 
-    cout << divisible(n,a,b);
+    int num,a,b;
+    cout << "Enter input for count of numbers divisible:" << endl;
+    cin >> num >> a >> b; 
+    cout << divisible(num,a,b) << endl;
+
+    int m,n;
+    cout << "Enter 2 numbers to find GCD:" << endl;
+    cin >> m >> n;
+    cout << gcd(max(m,n), min(m,n));
 
     return 0;
 }
