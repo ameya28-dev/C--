198cacd5cffdfbbbcdfa3971004f43182a362eef
Ameya G Mahale <ameya28.mahale@gmail.com>
2023-02-15 21:06:25 +0530

    Infix to postfix fixed

--- Prefix_Infix_Postfix.cpp
+++ Prefix_Infix_Postfix.cpp
@@ -16,7 +16,7 @@ string infixToPostfix(string s){
     stack<char> st;
     string res = "";
 
-    for(int i = 0; i <= s.size()-1; i++){
+    for(int i = 0; i <= s.length()-1; i++){
 
         if((s[i] >= 'a' && s[i] <= 'z' )|| (s[i] >= 'A' && s[i] <= 'Z')) res += s[i];
         else if(s[i] == '(') st.push(s[i]);
@@ -35,81 +35,17 @@ string infixToPostfix(string s){
             st.push(s[i]);
         }
 
-        while(!st.empty()){
+    }
+
+    while(!st.empty()){
             res += st.top();
             st.pop();
-        }
-
     }
+
 
     return res;
 }
 
-int prec(char c)
-{
-    if (c == '^')
-        return 3;
-    else if (c == '/' || c == '*')
-        return 2;
-    else if (c == '+' || c == '-')
-        return 1;
-    else
-        return -1;
-}
- 
-// The main function to convert infix expression
-// to postfix expression
-void infixToPostFix(string s)
-{
- 
-    stack<char> st; // For stack operations, we are using
-                    // C++ built in stack
-    string result;
- 
-    for (int i = 0; i < s.length(); i++) {
-        char c = s[i];
- 
-        // If the scanned character is
-        // an operand, add it to output string.
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
-            || (c >= '0' && c <= '9'))
-            result += c;
- 
-        // If the scanned character is an
-        // ‘(‘, push it to the stack.
-        else if (c == '(')
-            st.push('(');
- 
-        // If the scanned character is an ‘)’,
-        // pop and to output string from the stack
-        // until an ‘(‘ is encountered.
-        else if (c == ')') {
-            while (st.top() != '(') {
-                result += st.top();
-                st.pop();
-            }
-            st.pop();
-        }
- 
-        // If an operator is scanned
-        else {
-            while (!st.empty()
-                   && prec(s[i]) <= prec(st.top())) {
-                result += st.top();
-                st.pop();
-            }
-            st.push(c);
-        }
-    }
- 
-    // Pop all the remaining elements from the stack
-    while (!st.empty()) {
-        result += st.top();
-        st.pop();
-    }
- 
-    cout << result << endl;
-}
 
 int prefixEvaluation(string s){
     stack<int> st;
@@ -187,7 +123,7 @@ int main(){
     cout << postfixEvaluation("46+2/5*7+") << "\n\n";
 
     cout << "Convert Infix expression to Postfix :\n";
-    infixToPostFix("(a-b/c)*(a/k-l)");
+    cout << infixToPostfix("(a-b/c)*(a/k-l)") << "\n\n";
 
     return 0;
 }
