c773ce8f3ca9c11161363bd98173dfd32c21b3a2
Ameya Mahale <ameya28.mahale@gmail.com>
2023-02-19 00:22:41 +0530

    Queues using arrays

--- Queues.cpp
+++ Queues.cpp
@@ -32,11 +32,40 @@ class Queue{
             }
         }
 
+        void dequeue(){
+            if(front == -1 || front > back){
+                cout << "No element in the Queue\n";
+                return;
+            }
+            front++;
+        }
 
+        int peek(){
+            if(front == -1 || front > back){
+                cout << "No element in the Queue\n";
+                return -1;
+            }
+            return arr[front];
+        }
+
+        bool empty(){
+            if(front == -1 || front > back){
+                return true;
+            }
+            return false;
+        }
 
 };
 
 int main(){
+    Queue q = Queue();
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    q.enqueue(4);
+    q.dequeue();
+    cout << "Is this queue empty: "<< q.empty() <<"\n";
+    cout << "First element: "<< q.peek() <<"\n";
 
     return 0;
 }
